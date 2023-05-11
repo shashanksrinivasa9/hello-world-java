57bfcb1bd8bf92449c4ddd9f7cb113007e2d2620
Srinivasa Shashank Kolipaka <131415901+shashanksrinivasa9@users.noreply.github.com>
2023-05-10 18:00:06 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -37,6 +37,5 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
   }
 }
